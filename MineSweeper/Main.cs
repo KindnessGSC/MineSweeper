ae4e36d08000b47a4d6ccb776c78aef8cad82d94
Алина <alina.nickmail@gmail.com>
2022-11-27 19:54:37 +0300

    Открытие таблицы лидеров из главного меню

--- MineSweeper/Main.cs
+++ MineSweeper/Main.cs
@@ -73,7 +73,15 @@ namespace MineSweeper
 
 		private void MenuButton_Click(object sender, EventArgs e)
 		{
+           
 
-		}
-	}
+        }
+
+        private void Leaderboard_Click(object sender, EventArgs e)
+        {
+            //открытие таблицы лидеров
+            CSFormLeaderboards form = new CSFormLeaderboards();
+            form.ShowDialog();
+        }
+    }
 }
