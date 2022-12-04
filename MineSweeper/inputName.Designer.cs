85843b1083a0fec47b58aa4b549a82044f6650db
Object21 <Vladislav.aika@yandex.ru>
2022-12-04 21:45:22 +0300

    ввод имени

---
+++ MineSweeper/inputName.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace MineSweeper
+{
+	partial class inputName
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.NameTextBox = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// NameTextBox
+			// 
+			this.NameTextBox.Location = new System.Drawing.Point(89, 37);
+			this.NameTextBox.Name = "NameTextBox";
+			this.NameTextBox.Size = new System.Drawing.Size(269, 20);
+			this.NameTextBox.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "ваш ник:";
+			// 
+			// inputName
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(380, 97);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.NameTextBox);
+			this.Name = "inputName";
+			this.Text = "Введите имя";
+			this.Load += new System.EventHandler(this.inputName_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox NameTextBox;
+		private System.Windows.Forms.Label label1;
+	}
+}
